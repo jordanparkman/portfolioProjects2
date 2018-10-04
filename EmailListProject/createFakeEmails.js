8d7ce956a5457ac6913b90abc4831f580ac64619
Jordan Parkman <guiltygecko@yahoo.com>
2018-10-04 15:07:44 -0400

    Push EmailList Project

---
+++ EmailListProject/createFakeEmails.js
@@ -0,0 +1,26 @@
+var mysql = require('mysql');
+var faker = require("faker");
+
+var connection = mysql.createConnection({
+    host:'localhost',
+    user : 'guiltygecko',
+    database : 'join_us'
+});
+
+var data = [];
+for(var i = 0; i < 144; i++){
+    data.push([
+        faker.internet.email(),
+        faker.date.past()
+    ]);
+}
+ 
+ 
+var q = 'INSERT INTO users (email, created_at) VALUES ?';
+ 
+connection.query(q, [data], function(err, result) {
+  console.log(err);
+  console.log(result);
+});
+ 
+connection.end();
